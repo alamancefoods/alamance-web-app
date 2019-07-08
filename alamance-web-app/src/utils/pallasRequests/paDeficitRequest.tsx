c6156953cb780d53a417533dcf0da80c25991980
MTempleman <huntertempleman@gmail.com>
2019-07-08 01:33:30 -0600

    Corrected request URL
    PaDeficitRequest URL was incorrect for our purposes. Need to consider the
    benefits of proxying downloads. Leaning towards kicking that can, provided
    there's no obvious and immediate security risk.

--- alamance-web-app/src/utils/pallasRequests/paDeficitRequest.tsx
+++ alamance-web-app/src/utils/pallasRequests/paDeficitRequest.tsx
@@ -1,4 +1,4 @@
 import { pallasURL, pallasServerAddress, pallasAddress } from '../rootURLs'
 
-export const paDeficitRequest: string = pallasAddress.concat('/pa-deficits')
+export const paDeficitRequest: string = pallasServerAddress.concat('/pa-deficits')
 export const oldPaRequest = pallasAddress.concat('/pa-deficits')
